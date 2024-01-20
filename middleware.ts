eb895801824e8dfbfd716ad077a4fbddf104919c
Brandon Hancock <brandon@brandonhancock.io>
2024-01-20 16:44:56 -0500

    Fix spelling

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,7 @@ export default authMiddleware({
     "/api/run/retrieve",
     "/api/challenge-users",
     "/api/openai",
-    "/api/send-notification",
+    "/api/send-notifications",
   ],
 });
 
