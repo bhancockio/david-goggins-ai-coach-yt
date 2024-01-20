404377e463c7bd0af3f3e7790b9a4c433e655fcd
Brandon Hancock <brandon@brandonhancock.io>
2024-01-20 14:06:55 -0500

    more css on sign up/in

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex justify-center py-24">
+      <SignIn />
+    </div>
+  );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex justify-center py-24">
+      <SignUp />
+    </div>
+  );
 }
